d6aff3fbbb3698c61c372ca3a20a2b0369aa96ce
gustavogss <gustavogss.dev@gmail.com>
2025-06-29 21:47:56 -0300

    feat: QrCode generate create

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import prompt from "prompt"
-import {mainPrompt} from './prompts/prompt-main.js'
 import chalk from "chalk"
+import {mainPrompt} from './prompts/prompt-main.js'
+import { createQRCode } from "./services/qr-code/create.js"
 
 async function main() {
   prompt.get(mainPrompt, async function(_err, result) {
-    if (result.select == 1) console.log(chalk.green("Escolheu o qrCODE"))
+    if (result.select == 1) await createQRCode()
     if (result.select == 2) console.log(chalk.green("Escolheu o password"))
   })
   prompt.start()
